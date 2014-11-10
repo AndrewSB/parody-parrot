2e95da0210040e814f189ef1c5e337970be13779
AndrewSB <asbreckenridge@me.com>
2014-11-10 12:54:01 -0600

    understand that it IS a dict

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from twitter import *
+import json
 print "parody parrot"
 
 twitter_stream = TwitterStream(auth=OAuth("183299989-LE4WbYNKTLxibK5vx1meVMEdZpUjFQcfRoEdtkfX", "uhREnKogXj49HgqeX1uxvTC7WgEJHQkOVci2lKRrRGw7j", "zbsCXr4Bel5pxuHGi9KXcDFaR", "cgBUYOlgGKC1f4A57bn1iHnV3PlVq4B0nV13m98V2I3rpQonu7"))
@@ -6,4 +7,4 @@ twitter_stream = TwitterStream(auth=OAuth("183299989-LE4WbYNKTLxibK5vx1meVMEdZpU
 iterator = twitter_stream.statuses.sample()
 
 for tweet in iterator:
-	print tweet
+	print type(tweet)

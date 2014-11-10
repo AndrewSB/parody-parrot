42577c5fbaa210d020b86983a41f457b5e9c6ec9
AndrewSB <asbreckenridge@me.com>
2014-11-10 00:12:56 -0600

    add boilerplate twitter oauth

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
+from twitter import *
 print "parody parrot"
+
+t = Twitter(
+    auth=OAuth(token, token_key, con_secret, con_secret_key)))
+
+# Get your "home" timeline
+t.statuses.home_timeline()

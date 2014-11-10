847a577443735e5748be3bcf1e1c15272d1bd6af
AndrewSB <asbreckenridge@me.com>
2014-11-10 01:28:26 -0600

    can access tweet

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 from twitter import *
 print "parody parrot"
 
-t = Twitter(
-    auth=OAuth(token, token_key, con_secret, con_secret_key)))
+twitter_stream = TwitterStream(auth=OAuth("183299989-LE4WbYNKTLxibK5vx1meVMEdZpUjFQcfRoEdtkfX", "uhREnKogXj49HgqeX1uxvTC7WgEJHQkOVci2lKRrRGw7j", "zbsCXr4Bel5pxuHGi9KXcDFaR", "cgBUYOlgGKC1f4A57bn1iHnV3PlVq4B0nV13m98V2I3rpQonu7"))
 
-# Get your "home" timeline
-t.statuses.home_timeline()
+iterator = twitter_stream.statuses.sample()
+
+for tweet in iterator:
+	print tweet
